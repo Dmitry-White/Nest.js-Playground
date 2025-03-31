423c44f79d63aad3fb9237a2463c84180885b7fd
Dmitry White <dmitry.b.white@gmail.com>
2025-03-31 15:06:52 +0100

    Add Fundamentals TypeORM Coffee entity

--- fundamentals/src/coffees/entities/coffee.entity.ts
+++ fundamentals/src/coffees/entities/coffee.entity.ts
@@ -1,7 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
 }
 
