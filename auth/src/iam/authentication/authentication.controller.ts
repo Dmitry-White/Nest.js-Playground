79a0f0a58bdb28068fc68556ea4a061c07acb0bc
Dmitry White <dmitry.b.white@gmail.com>
2025-04-05 19:24:19 +0100

    Add Auth IAM JWT cookie

--- auth/src/iam/authentication/authentication.controller.ts
+++ auth/src/iam/authentication/authentication.controller.ts
@@ -1,4 +1,6 @@
+import { Res } from '@nestjs/common';
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+
 import { AuthenticationService } from './authentication.service';
 import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
@@ -16,5 +18,16 @@ export class AuthenticationController {
   @Post('sign-in')
   signIn(@Body() signInDto: SignInDto) {
     return this.authenticationService.signIn(signInDto);
+  }
+
+  @HttpCode(HttpStatus.OK) // by default @Post does 201, we wanted 200 - hence using @HttpCode(HttpStatus.OK)
+  @Post('sign-in-cookie')
+  async signInCookie(@Body() signInDto: SignInDto, @Res({ passthrough: true }) response) {
+    const accessToken = await this.authenticationService.signIn(signInDto);
+    response.cookie('accessToken', accessToken, {
+      secure: true,
+      httpOnly: true,
+      sameSite: true,
+    });
   }
 }
