38204b428147fc5605a583d4eec0f63d0ef91274
Dmitry White <dmitry.b.white@gmail.com>
2025-04-19 15:06:34 +0100

    Add Advanced Mixins ORM setup

--- advanced/src/app.module.ts
+++ advanced/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,11 +12,26 @@ import { ThreadsModule } from './threads/threads.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SchedulerModule,
     CoffeesModule,
     RewardsModule,
     ThreadsModule,
     HttpClientModule.register({ baseUrl: 'http://nestjs.com' }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow('DB_HOST'),
+        port: configService.getOrThrow('DB_PORT'),
+        database: configService.getOrThrow('DB_DATABASE'),
+        username: configService.getOrThrow('DB_USERNAME'),
+        password: configService.getOrThrow('DB_PASSWORD'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
